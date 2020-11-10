f9633b4d21db0795ce370d103704df0674599f80
naitik2314 <shahnaitik2000@gmail.com>
2020-11-10 19:25:01 +0530

    Importing cloud_firestore.dart

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -2,6 +2,7 @@ import 'package:chatapp/widgets/auth/auth_form.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class AuthScreen extends StatefulWidget {
   @override

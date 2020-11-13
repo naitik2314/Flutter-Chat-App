4100c45d086ee2f2b123c4ed55ed476d057e7f8d
naitik2314 <shahnaitik2000@gmail.com>
2020-11-13 07:26:10 +0530

    _isLoading

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -11,6 +11,7 @@ class AuthScreen extends StatefulWidget {
 
 class _AuthScreenState extends State<AuthScreen> {
   final _auth = FirebaseAuth.instance;
+  var _isLoading = false;
 
   void _submitAuthForm(String email, String password, String username,
       bool isLogin, BuildContext ctx) async {

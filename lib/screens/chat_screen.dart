64742367608208f5e010f406b01d21369af22c9b
naitik2314 <shahnaitik2000@gmail.com>
2020-11-14 12:33:18 +0530

    SizedBox

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -20,6 +20,7 @@ class ChatScreen extends StatelessWidget {
                 child: Row(
                   children: <Widget>[
                     Icon(Icons.exit_to_app),
+                    SizedBox(width: 8),
                     Text('Logout'),
                   ],
                 ),

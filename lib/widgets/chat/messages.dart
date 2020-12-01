e52d5fd725db4e433eb040f7246bd7a613271d1c
naitik2314 <shahnaitik2000@gmail.com>
2020-12-01 12:28:56 +0530

    Messages

---
+++ lib/widgets/chat/messages.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Messages extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder();
+  }
+}

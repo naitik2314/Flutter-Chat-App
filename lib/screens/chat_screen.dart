105801b2e9b4c965a062a86a69ca5929ec2bf399
naitik2314 <shahnaitik2000@gmail.com>
2020-12-02 12:43:09 +0530

    Remove unused import.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,7 +2,6 @@ import 'package:chatapp/widgets/chat/messages.dart';
 import 'package:chatapp/widgets/chat/new_message.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class ChatScreen extends StatelessWidget {

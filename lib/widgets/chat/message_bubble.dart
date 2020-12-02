6e3620eae9b6d9b935150b920c7e68f9cf1e478f
naitik2314 <shahnaitik2000@gmail.com>
2020-12-02 14:45:20 +0530

    child text Message

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -4,15 +4,17 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).accentColor,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        width: 140,
-        padding: EdgeInsets.symmetric(
-          vertical: 10,
-          horizontal: 16,
-        ),
-        margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8));
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      width: 140,
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 16,
+      ),
+      margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
+      child: Text('Message'),
+    );
   }
 }

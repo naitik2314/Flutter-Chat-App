8886b4ce8e4a1af498b8b78f4077ea8d96528a5b
naitik2314 <shahnaitik2000@gmail.com>
2020-12-02 14:40:49 +0530

    Border radius.

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -4,7 +4,10 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(color: Theme.of(context).accentColor),
+      decoration: BoxDecoration(
+        color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.circular(12),
+      ),
     );
   }
 }

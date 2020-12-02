6df6a1cbf1865640cac4e363ad3c62956d62ca7d
naitik2314 <shahnaitik2000@gmail.com>
2020-12-02 14:39:39 +0530

    accent color for buble message.

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -4,7 +4,7 @@ class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(),
+      decoration: BoxDecoration(color: Theme.of(context).accentColor),
     );
   }
 }

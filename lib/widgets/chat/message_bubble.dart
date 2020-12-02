62d27dcc06603a875b76b1f760c34be1bd1f4563
naitik2314 <shahnaitik2000@gmail.com>
2020-12-02 14:38:39 +0530

    BoxDecoration..

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class MessageBubble extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      decoration: BoxDecoration(),
+    );
   }
 }

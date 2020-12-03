c1cfe5aa2b86a30518f8ddd787887039de5f7b99
naitik2314 <shahnaitik2000@gmail.com>
2020-12-03 11:50:36 +0530

    ChatApp

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -21,8 +21,10 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) =>
-              MessageBubble(chatDocs[index]['text'], chatDocs[index]['userId']),
+          itemBuilder: (ctx, index) => MessageBubble(
+            chatDocs[index]['text'],
+            chatDocs[index]['userId'],
+          ),
         );
       },
     );

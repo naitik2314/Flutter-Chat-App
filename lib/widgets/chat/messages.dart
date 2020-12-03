48a058af012da5eedcd3cea312da97c3ed30c174
naitik2314 <shahnaitik2000@gmail.com>
2020-12-03 12:36:51 +0530

    futureSnapshot.data.uid,

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -32,7 +32,7 @@ class Messages extends StatelessWidget {
                 itemCount: chatDocs.length,
                 itemBuilder: (ctx, index) => MessageBubble(
                   chatDocs[index]['text'],
-                  chatDocs[index]['userId'],
+                  chatDocs[index]['userId'] == futureSnapshot.data.uid,
                 ),
               );
             });

a12087367d0cc151a542c633255a4f16770bcd5e
naitik2314 <shahnaitik2000@gmail.com>
2020-12-07 12:28:54 +0530

    Image Picker

---
+++ lib/widgets/pickers/user_image_picker.dart
@@ -0,0 +1,11 @@
+class UserImagePicker extends StatefulWidget {
+  @override
+  _UserImagePickerState createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

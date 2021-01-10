81a827c03e1b3422bc7a373c46ef65072b688bda
naitik2314 <shahnaitik2000@gmail.com>
2021-01-10 12:47:58 +0530

    Picked image file widget.

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -3,6 +3,8 @@ import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 
 class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+  final void Function(File pickedImage) imagePickFn;
   @override
   _UserImagePickerState createState() => _UserImagePickerState();
 }
@@ -16,6 +18,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImage = pickedImageFile;
     });
+    widget.imagePickFn(pickedImageFile);
   }
 
   @override

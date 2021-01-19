056d3d3e8a60498167fd7ce5ad6e7c4fd1a515e5
naitik2314 <shahnaitik2000@gmail.com>
2021-01-19 17:41:30 +0530

    Changed the top to 0 again.

--- lib/widgets/chat/message_bubble.dart
+++ lib/widgets/chat/message_bubble.dart
@@ -68,7 +68,7 @@ class MessageBubble extends StatelessWidget {
           ],
         ),
         Positioned(
-            top: -10,
+            top: 0,
             left: isMe ? null : 120,
             right: isMe ? 120 : null,
             child: CircleAvatar(
